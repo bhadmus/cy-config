d33f557b26f495b562fa4fc281c8b98b0a887d94
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-08-03 17:18:56 +0100

    refactored gitignore

--- src/templates/prompter.js
+++ src/templates/prompter.js
@@ -4,7 +4,7 @@ import inquirer from "inquirer";
 
 export async function collectInfo() {
 
-  const version = "2.0.0"; 
+  const version = "2.0.1"; 
 
   // Check command-line arguments for --version or --help
   const args = process.argv.slice(2);
--- src/utils/ignore.js
+++ src/utils/ignore.js
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
 
-import fs from 'fs-extra';
-import path from 'path';
+import fs from "fs-extra";
+import path from "path";
 
 export async function createGitIgnoreFile(answers) {
-    const ignorePath = path.join(process.cwd(), '.gitignore');
+  if (answers.ignoreFile) {
+    const ignorePath = path.join(process.cwd(), ".gitignore");
 
     // Define the content with no leading whitespace
     const ignoreContent = `# node_modules
-node_modules/
-# MacOS file
-.DS_Store
-# Jetbrains generated file
-.idea/
-# VsCode generated file
-.vscode/
-# Cypress reports
-cypress/videos/
-cypress/screenshots/`;
+    node_modules/
+    # MacOS file
+    .DS_Store
+    # Jetbrains generated file
+    .idea/
+    # VsCode generated file
+    .vscode/
+    # Cypress reports
+    cypress/videos/
+    cypress/screenshots/`;
 
     // Write .gitignore file as plain text
-    await fs.writeFile(ignorePath, ignoreContent, 'utf8');
+    await fs.writeFile(ignorePath, ignoreContent, "utf8");
 
-    console.log('Created .gitignore');
+    console.log("Created .gitignore");
+  }
 }
